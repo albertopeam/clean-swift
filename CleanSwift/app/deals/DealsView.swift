48deed3b7aee62ccb3ab78e48bdb43df647d3d78
Penas Amor, Alberto <alberto.penas.amor@accenture.com>
2017-12-18 10:11:04 +0100

    fixed cell layout

--- CleanSwift/app/deals/DealsView.swift
+++ CleanSwift/app/deals/DealsView.swift
@@ -9,10 +9,11 @@
 import Foundation
 import UIKit
 
-class DealsView:UIView, UICollectionViewDataSource{
+class DealsView:UIView, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     @IBOutlet weak var collectionView: UICollectionView!
     fileprivate var deals:Array<Deal> = Array()
+    fileprivate let insets = UIEdgeInsets(top: 0.0, left: 16.0, bottom: 16.0, right: 10.0)
     
     func reloadData(deals:Array<Deal>) -> Void {
         self.deals = deals
@@ -30,10 +31,27 @@ class DealsView:UIView, UICollectionViewDataSource{
     
     func collectionView(_ collectionView: UICollectionView,
                         cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell:DealCell = collectionView.dequeueReusableCell(withReuseIdentifier: "deal_cell",
-                                                               for: indexPath) as! DealCell
+        let cell:DealCell = collectionView.dequeueReusableCell(withReuseIdentifier: "deal_cell", for: indexPath) as! DealCell
         let deal:Deal = deals[indexPath.row]
         cell.setup(deal: deal)
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: self.frame.width, height: 132)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        insetForSectionAt section: Int) -> UIEdgeInsets {
+        return insets
+    }
+  
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return insets.top
     }
 }
