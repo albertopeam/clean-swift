9e37b15d827470b2822be783974994b3bd646168
Penas Amor, Alberto <alberto.penas.amor@accenture.com>
2017-12-19 16:45:19 +0100

    updated UI updated Deal model

---
+++ CleanSwift/gateway/network/deals/SearchDealsService.swift
@@ -0,0 +1,9 @@
+//
+//  SearchDealsService.swift
+//  CleanSwift
+//
+//  Created by Penas Amor, Alberto on 19/12/17.
+//  Copyright © 2017 Penas Amor, Alberto. All rights reserved.
+//
+
+import Foundation
